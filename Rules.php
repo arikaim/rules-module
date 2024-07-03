1789acbfafdaac614f1ae4bd6348965afeb489c2
arikaim <info@arikaim.com>
2024-07-03 07:20:56 -0400

    =

---
+++ Rules.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c) Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Modules\Rules;
+
+use Arikaim\Core\Extension\Module;
+
+/**
+ * Rules module class
+ */
+class Rules extends Module
+{
+    /**
+     * Install module
+     *
+     * @return void
+     */
+    public function install()
+    {        
+    }
+}
